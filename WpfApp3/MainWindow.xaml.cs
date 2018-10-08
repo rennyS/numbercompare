c5be3a057e73ad41608d94b1146033e024e00e53
Laurence <laurence.mackie@hotmail.com>
2018-10-08 16:31:02 +0100

    Signed-off-by: Laurence <laurence.mackie@hotmail.com>

--- WpfApp3/MainWindow.xaml.cs
+++ WpfApp3/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WpfApp3
+namespace numbercompare
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
